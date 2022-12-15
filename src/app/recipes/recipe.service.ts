77cea7a319239860ee6b1a82bcb508d06a69a7a0
Luis Rodriguez <luisu_10@hotmail.com>
2022-12-14 22:29:38 -0600

    Added forms to project

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 // import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
@@ -6,6 +7,7 @@ import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService{
+  recipesChanged = new Subject<Recipe[]>();
     // recipeSelected = new EventEmitter<Recipe>();
     // recipeSelected = new Subject<Recipe>();
     private recipes: Recipe[] = [
@@ -30,5 +32,20 @@ export class RecipeService{
 
     addIngredientsToShoppingList(ingredients: Ingredient[]){
         this.shoppingListService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe){
+      this.recipes.push(recipe);
+      this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe){
+      this.recipes[index] = newRecipe;
+      this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number){
+      this.recipes.splice(index, 1);
+      this.recipesChanged.next(this.recipes.slice());
     }
 }
